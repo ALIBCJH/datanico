8f5870c6e8595645fc754c677c342799bb5faad9
ALIGCJH <simonjuma465@gmail.com>
2025-07-29 19:57:39 +0300

    heros image

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,15 +1,26 @@
 import { Link } from "react-router-dom";
-import hero from "../assets/tech/hero.webp";
+import heroDesktop from "../assets/tech/hero.webp";
+import heroMobile from "../assets/tech/datanico2.jpg";
 
 const Hero = () => {
   return (
-    <section className=" w-full bg-white  relative">
+    <section className="w-full bg-white relative">
+      {/* Mobile Image */}
       <img
-        src={hero}
-        alt="Hero"
-        className=""
+        src={heroMobile}
+        alt="Hero Mobile"
+        className="block md:hidden w-full h-auto object-cover"
       />
-      <div className=" absolute top-[80%] sm:left-[60px]   lg:left-[140px]  z-20">
+
+      {/* Desktop Image */}
+      <img
+        src={heroDesktop}
+        alt="Hero Desktop"
+        className="hidden md:block w-full h-auto object-cover"
+      />
+
+      {/* Overlay Button */}
+      <div className="absolute top-[80%] sm:left-[60px] lg:left-[140px] z-20">
         <Link to="/quote">
           <button className="bg-[#FF770F] text-white text-base sm:text-lg font-semibold px-6 sm:px-12 py-3 rounded-full shadow-md hover:bg-[#FF770F] hover:scale-105 transition-transform duration-300">
             Get Quote
